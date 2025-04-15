60d2bd3f5ac6f420b4669f4b4b28b107abb21922
certifiedprogamer <sowerskerry2@gmail.com>
2025-04-15 09:20:31 -0400

    changes to longestcommonprefix so it gets accepted by the submit button

--- longestcommonprefix.py
+++ longestcommonprefix.py
@@ -6,7 +6,7 @@ If there is no common prefix, return an empty string ""."""
 
 
 class Solution(object):
-    def longestCommonPrefix(self, strs: list):
+    def longestCommonPrefix(self, strs):
         for i in range(len(strs)):
             strs[i] = list(strs[i])
         shortest_word = strs[0]
@@ -15,11 +15,16 @@ class Solution(object):
                 shortest_word = strs[i]
         strs.remove(shortest_word)
         prefix = ""
+        if len(strs) == 0:
+            for i in shortest_word:
+                prefix += i
+            return prefix
         for letter_indx in range(len(shortest_word)):
             for i in strs:
                 if shortest_word[letter_indx] != i[letter_indx]:
                     return prefix
             prefix += shortest_word[letter_indx]
+        return prefix
 
 
 print(Solution.longestCommonPrefix(1, ["flower", "flow", "flight"]))
